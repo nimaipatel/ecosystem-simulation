5c6a281ab0b538bae8c84b0516fc6adf5447c79a
nimai <nimai.m.patel@gmail.com>
2020-05-18 17:14:53 +0530

    Implemented maximum health

--- animal.js
+++ animal.js
@@ -2,8 +2,8 @@ class Animal {
     constructor() {
         this.x = random(5, width-5)
         this.y = random(5, height-5)
-        //tweak health 
-        this.health = randomGaussian(80, 5)
+        this.maxHealth = randomGaussian(80, 5)
+        this.health = this.maxHealth*0.8
         console.log(this.health)
     }
 
@@ -31,13 +31,12 @@ class Animal {
     eat(food) {
             if (dist(this.x, this.y, food.x, food.y) < 20) {
             //console.log('eating')
-            if (this.health += food.nutrition > 100){
-                this.health = 100
+            if (this.health += food.nutrition > this.maxHealth){
+                this.health = this.maxHealth
             } else{
                 this.health += food.nutrition
             }
             food.x = food.y = null
-            console.error(this.health)
         }
     }
 }
