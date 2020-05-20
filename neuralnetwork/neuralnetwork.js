ede2ffa905a691bc9dc37b5d6d28c46b6c6734f5
nimai <nimai.m.patel@gmail.com>
2020-05-20 07:07:17 +0530

    added forward pass but still need to debug

--- neuralnetwork/neuralnetwork.js
+++ neuralnetwork/neuralnetwork.js
@@ -2,21 +2,37 @@ class NeuralNetwork {
     constructor(...neurons) {
         this.neurons = neurons
         this.numberOfLayers = neurons.length
+        this.layers =[]
         this.weights = []
         this.biases = []
+
+        //creating layers, which are just matrices with activations as their elements
+        // for(let neuron of neurons){
+        //     this.layers.push(new Matrix(neuron, 1))
+        // }
+
+        //creating weights matirces and adding them to weights array
         for (let i=0; i < this.numberOfLayers - 1; i++){
-            let weightMatrix = new Matrix(neurons[i], neurons[i+1])
+            let weightMatrix = new Matrix(neurons[i+1], neurons[i])
             weightMatrix.fillRandom()
             this.weights.push(weightMatrix)
         }
-
+        
+        //creating biases matirces and adding them to biases array
         for (let i=0; i<this.numberOfLayers-1; i++){
-            let biasMatix = new Matrix(1, neurons[i+1])
+            let biasMatrix = new Matrix(neurons[i+1], 1)
+            biasMatrix.fillRandom()
+            this.biases.push(biasMatrix)
         }
     }
 
     forwardPass(input){
-        
+        input = Matrix.arrayToMatrix(input)
+        for(let i=0; i<this.numberOfLayers -1; i++){
+            input = Matrix.multiply(this.weights[i], input)
+            input.add(this.biases[i])
+        }
+        return input
     }
 }
 
