f5fe5619490e425e7d3c5f08401b02562a7da65f
nimai <nimai.m.patel@gmail.com>
2020-05-21 12:44:48 +0530

    added ROV style, but not implemented yet

--- animal.js
+++ animal.js
@@ -4,22 +4,26 @@ class Animal {
         this.y = random(5, height - 5)
         this.maxHealth = randomGaussian(80, 5)
         this.health = this.maxHealth * 0.8
+        this.radiusOfVision = randomGaussian(50, 5)
         //console.log(this.health)
     }
 
     show() {
+        strokeWeight(0)
+        fill(51, 100, 100, 0.4)
+        circle(this.x, this.y, this.radiusOfVision * 2)
+
         textAlign(CENTER)
         textSize(this.size)
         text(this.emoji, this.x, this.y)
 
-        //strokeWeight(0)
+        strokeWeight(0.5)
         fill(51)
         rect(this.x, this.y + 10, 25, 5, 5)
 
-        //strokeWeight(0)
+        strokeWeight(0.5)
         fill(90.5, 100, 49.4)
         rect(this.x - 25 * (1 - this.health / this.maxHealth), this.y + 10, 25 * this.health / this.maxHealth, 5, 5)
-
     }
 
     moveManual() {
@@ -48,16 +52,36 @@ class Animal {
         } else if (this.x < 0) {
             this.x = width - 5
         } else {
-            let prob = random(0, 1)
-            if (prob < 0.25) {
-                this.y -= this.speed
-            } else if (prob < 0.50) {
-                this.y += this.speed
-            } else if (prob < 0.75) {
-                this.x -= this.speed
-            } else {
-                this.x += this.speed
+            let direction = random(0, 360)
+            this.x += this.speed * cos(direction)
+            this.y += this.speed * sin(direction)
+            console.log(direction, sin(direction))
+        }
+    }
+
+    findClosest(foods) {
+        let closest = foods[0]
+        for (let i = 0; i < foods.length; i++) {
+            if (dist(this.x, this.y, foods[i].x, foods[i].y) < dist(this.x, this.y, closest.x, closest.y)) {
+                closest = foods[i]
             }
+        }
+        return closest
+    }
+
+    moveTowards(food) {
+        if (this.y > height) {
+            this.y = 5
+        } else if (this.y < 0) {
+            this.y = height - 5
+        } else if (this.x > width) {
+            this.x = 5
+        } else if (this.x < 0) {
+            this.x = width - 5
+        } else {
+            let direction = atan((food.y - this.y) / (food.x - this.x))
+            this.x += this.speed * cos(direction)
+            this.y += this.speed * sin(direction)
         }
     }
 
@@ -69,7 +93,7 @@ class Animal {
             } else {
                 this.health += food.nutrition
             }
-            food.x = food.y = null
+            food.remove()
         }
     }
 }
