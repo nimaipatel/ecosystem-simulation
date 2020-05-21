e0e71658dc6593418f6d3d5446bee21612a61e33
nimai <nimai.m.patel@gmail.com>
2020-05-21 08:17:19 +0530

    Added stopped button, may still need some work tho

--- settings.js
+++ settings.js
@@ -5,15 +5,23 @@ let foxesInput = document.getElementById('foxes')
 let rabbitsInput = document.getElementById('rabbits')
 let foodsInput = document.getElementById('foods')
 
+// foxesInput.addEventListener("input", () =>{
+//     if()
+// })
+
 startButton.addEventListener("click", () =>{
     initialize()
-    console.log("ran init")
-    //numberOfFoxes = foxesInput.value
-    //numberOfRabbits = rabbitsInput.value
+    //console.log("ran initializer")
 })
 
 stopButton.addEventListener("click", () =>{
-    //= false
+    foxes = []
+    rabbits = []
+    foods = []
+    foxesInput.value = 0
+    rabbitsInput.value = 0
+    foodsInput.value = 0
+    initialize()
     //console.log(running)
 })
 
